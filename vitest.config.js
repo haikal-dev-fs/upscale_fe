8f65cdf8cb58dd53fc3513a6819c3e835c008729
Haikal <mhaikalas@gmail.com>
2025-07-14 18:25:10 +0700

    update vitest config

--- vitest.config.js
+++ vitest.config.js
@@ -1,8 +1,8 @@
-import { fileURLToPath } from 'node:url'
-import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
+import { defineConfig } from 'vitest/config'
 import viteConfig from './vite.config'
 
 export default defineConfig({
+  ...viteConfig,
   test: {
     environment: 'jsdom',
     setupFiles: 'vitest.setup.js',
