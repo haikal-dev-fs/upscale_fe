2c1b8e163b18dc722c44692891051b18033f7cf5
Haikal <mhaikalas@gmail.com>
2025-07-14 18:21:59 +0700

    update login test

--- test/login.test.js
+++ test/login.test.js
@@ -1,11 +1,10 @@
 import { mount } from '@vue/test-utils'
-import { testingPinia } from '../vitest.setup'
 import Login from '@/views/Login.vue'
 
 describe('Login', () => {
   it('renders', () => {
     const wrapper = mount(Login, {
-      global: { plugins: [testingPinia()] },
+      global: {},
     })
     expect(wrapper.find('h3').text()).toBe('Login')
   })
